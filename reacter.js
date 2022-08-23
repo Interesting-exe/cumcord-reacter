a514ff90ba7c81dcee8b746bb9ffd6e2ed260a07
Interesting <ozshwartz057@gmail.com>
2022-08-23 21:59:36 +0300

    test test

--- reacter.js
+++ reacter.js
@@ -39,7 +39,7 @@ const removeCommand = cumcord.commands.addCommand({
 export default {
     onLoad() {
         //let emojis = Object.values(getGuilds()).flatMap(g => g.id);
-        let emojis = Object.values(getGuildEmoji());
+        let emojis = Object.values(getGuildEmoji().getGuilds());
         log("emojis: "+emojis);
     },
     onUnload() {},
