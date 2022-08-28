7c9aff82ea26cf5d634418bcff271e5da44d3cdd
Interesting-exe <ozshwartz057@gmail.com>
2022-08-28 11:50:13 +0300

    testing

--- reacter.js
+++ reacter.js
@@ -24,7 +24,7 @@ const removeCommand = cumcord.commands.addCommand({
     handler: (ctx, send) => {
 
         let results = [];
-        let servers = getGuilds().map(g => g.id);
+        let servers = getGuilds();
         // for (let server of servers)
         // {
         //     let emojis = getGuildEmoji(server);
